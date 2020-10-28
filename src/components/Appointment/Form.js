6be45422098e11d282d7f7a5eb9fb1b654362de9
Paige Lindahl <paigemarlindahl@gmail.com>
2020-10-27 19:53:27 -0700

    story book components for scheduler

---
+++ src/components/Appointment/Form.js
@@ -0,0 +1,56 @@
+import React, { useState } from 'react';
+import InterviewerList from "components/InterviewerList";
+import Button from "components/Button";
+
+
+const Form = function(props) {
+  const [name, setName] = useState(props.name || "");
+  const [interviewer, setInterviewer] = useState(props.interviewer || null); 
+  
+  const reset = function() {
+    setName("");
+    setInterviewer(null);
+  }
+
+  const cancel = function() {
+    reset();
+    props.onCancel();
+
+  } 
+
+  return (
+    <main className="appointment__card appointment__card--create">
+      <section className="appointment__card-left">
+        <form 
+        onSubmit={event => event.preventDefault()}
+        autoComplete="off"
+        >
+          <input
+            className="appointment__create-input text--semi-bold"
+            name="name"
+            type="text"
+            placeholder="Enter student name"
+            value={name}
+            onChange={(event) => setName(event.target.value)}
+            /*
+              This must be a controlled component
+            */
+          />
+        </form>
+        <InterviewerList 
+        interviewers={props.interviewers} 
+        value={interviewer} 
+        onChange={setInterviewer} 
+        />
+      </section>
+      <section className="appointment__card-right">
+        <section className="appointment__actions">
+          <Button onClick={cancel} danger>Cancel</Button>
+          <Button onClick={props.onSave} confirm>Save</Button>
+        </section>
+      </section>
+    </main>
+  );
+}
+
+export default Form;
---
+++ src/components/DayListItem.js
@@ -0,0 +1,25 @@
+import React from "react";
+import classnames from "classnames";
+import "components/DayListItem.scss";
+
+export default function DayListItem(props) {
+  const dayClass = classnames("day-list__item", {
+    "day-list__item--selected": props.selected,
+    "day-list__item--full": (props.spots === 0)
+  })
+  const formatSpots = function(props) {
+    return props.spots === 0
+    ? "no spots remaining"
+    : props.spots === 1
+    ? "1 spot remaining"
+    : `${props.spots} spots remaining`;
+    
+  }
+  return (
+    <li onClick={() => props.setDay(props.name)} className={dayClass}>
+      <h2 className="text--regular">{props.name}</h2> 
+      <h3 className="text--light">{formatSpots(props)}</h3>
+    </li>
+  );
+}
+
